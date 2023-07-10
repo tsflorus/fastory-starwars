f1d10e8c3e58ae32ea87f6124f6075ad1bead129
Thomas Singerlé-Florus <t.singerleflorus@gmail.com>
2023-07-10 16:51:42 +0200

    style: resize searchbar

--- app/src/pages/SearchScreen/SearchScreen.tsx
+++ app/src/pages/SearchScreen/SearchScreen.tsx
@@ -40,109 +40,110 @@ const SearchScreen = () => {
   }, [debouncedSearchValue])
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center mt-20 w-8/12 mx-auto">
       {error && <h2>{error}</h2>}
-      <h1 className="font-jedi-outlined">Search</h1>
-        <div className='form-group'>
-          <label htmlFor='nameToSearch'>Name to search</label>
-          <input
-            type='nameToSearch'
-            className="bg-black border-2 border-yellow"
-            value={searchValue}
-            onChange={handleInputChange}
-            required
-          />
-          <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filtres</label>
-          <select id="countries" className="bg-black border-2 border-yellow" value={filter} onChange={(e) => setFilter(e.target.value)}>
-            <option value="">All</option>
-            <option value="people">People</option>
-            <option value="planets">Planets</option>
-            <option value="films">Films</option>
-            <option value="species">Species</option>
-            <option value="vehicles">Vehicles</option>
-            <option value="starships">Starships</option>
-          </select>
-        </div>
+      <h1 className="font-jedi-outlined text-4xl text-yellow text-center">Rechercher dans la base de l'empire</h1>
+      <input
+        placeholder='Nom à rechercher'
+        type='nameToSearch'
+        className='border-white bg-black p-2 font-starJedi rounded w-8/12 text-center my-10'
+        style={{borderWidth: 1}}
+        value={searchValue}
+        onChange={handleInputChange}
+        required
+      />
+      <label htmlFor="countries"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filtres</label>
+      <select id="countries" className="bg-black border-2 border-yellow" value={filter}
+              onChange={(e) => setFilter(e.target.value)}>
+        <option value="">All</option>
+        <option value="people">People</option>
+        <option value="planets">Planets</option>
+        <option value="films">Films</option>
+        <option value="species">Species</option>
+        <option value="vehicles">Vehicles</option>
+        <option value="starships">Starships</option>
+      </select>
       {loading && <p>Loading</p>}
 
-        <div style={{marginLeft: '5vw', marginRight: '5vw'}}>
-          <div>
-            <h1>People</h1>
-            {searchResult.people?.length && searchResult.people.map((person: Person, p: Key | null | undefined) => {
-              return (
-                <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(person.url, 'people')}>
-                  <p>{person.name}</p>
-                  <p>{person.height}</p>
-                  <p>{person.mass}</p>
-                  <p>{person.birth_year}</p>
-                </div>
-              )
-            })}
-          </div>
-          <div>
-            <h1>Planets</h1>
-            {searchResult.planets?.length && searchResult.planets.map((planet: Planet, p: Key | null | undefined) => {
-              return (
-                <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(planet.url, 'planets')}>
-                  <p>{planet.name}</p>
-                  <p>{planet.climate}</p>
-                  <p>{planet.terrain}</p>
-                  <p>{planet.population}</p>
-                </div>
-              )
-            })}
-          </div>
-          <div>
-            <h1>Films</h1>
-            {searchResult.films?.length && searchResult.films.map((film: Film, p: Key | null | undefined) => {
-              return (
-                <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(film.url, 'films')}>
-                  <p>{film.title}</p>
-                  <p>{film.director}</p>
-                  <p>{film.producer}</p>
-                </div>
-              )
-            })}
-          </div>
-          <div>
-            <h1>Species</h1>
-            {searchResult.species?.length && searchResult.species.map((specie: Species, p: Key | null | undefined) => {
-              return (
-                <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(specie.url, 'species')}>
-                  <p>{specie.name}</p>
-                  <p>{specie.language}</p>
-                  <p>{specie.classification}</p>
-                </div>
-              )
-            })}
-          </div>
-          <div>
-            <h1>Vehicles</h1>
-            {searchResult.vehicles?.length && searchResult.vehicles.map((vehicle: Vehicle, p: Key | null | undefined) => {
-              return (
-                <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(vehicle.url, 'vehicles')}>
-                  <p>{vehicle.name}</p>
-                  <p>{vehicle.vehicle_class}</p>
-                  <p>{vehicle.model}</p>
-                </div>
-              )
-            })}
-          </div>
-          <div>
-            <h1>Starships</h1>
-            {searchResult.starships?.length && searchResult.starships.map((starship: Starship, p: Key | null | undefined) => {
-              return (
-                <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(starship.url, 'starships')}>
-                  <p>{starship.name}</p>
-                  <p>{starship.manufacturer}</p>
-                  <p>{starship.model}</p>
-                  <p>{starship.passengers}</p>
-                </div>
-              )
-            })}
-          </div>
+      <div style={{marginLeft: '5vw', marginRight: '5vw'}}>
+        <div>
+          <h1>People</h1>
+          {searchResult.people?.length && searchResult.people.map((person: Person, p: Key | null | undefined) => {
+            return (
+              <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(person.url, 'people')}>
+                <p>{person.name}</p>
+                <p>{person.height}</p>
+                <p>{person.mass}</p>
+                <p>{person.birth_year}</p>
+              </div>
+            )
+          })}
         </div>
-    </>
+        <div>
+          <h1>Planets</h1>
+          {searchResult.planets?.length && searchResult.planets.map((planet: Planet, p: Key | null | undefined) => {
+            return (
+              <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(planet.url, 'planets')}>
+                <p>{planet.name}</p>
+                <p>{planet.climate}</p>
+                <p>{planet.terrain}</p>
+                <p>{planet.population}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <h1>Films</h1>
+          {searchResult.films?.length && searchResult.films.map((film: Film, p: Key | null | undefined) => {
+            return (
+              <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(film.url, 'films')}>
+                <p>{film.title}</p>
+                <p>{film.director}</p>
+                <p>{film.producer}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <h1>Species</h1>
+          {searchResult.species?.length && searchResult.species.map((specie: Species, p: Key | null | undefined) => {
+            return (
+              <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(specie.url, 'species')}>
+                <p>{specie.name}</p>
+                <p>{specie.language}</p>
+                <p>{specie.classification}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <h1>Vehicles</h1>
+          {searchResult.vehicles?.length && searchResult.vehicles.map((vehicle: Vehicle, p: Key | null | undefined) => {
+            return (
+              <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(vehicle.url, 'vehicles')}>
+                <p>{vehicle.name}</p>
+                <p>{vehicle.vehicle_class}</p>
+                <p>{vehicle.model}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <h1>Starships</h1>
+          {searchResult.starships?.length && searchResult.starships.map((starship: Starship, p: Key | null | undefined) => {
+            return (
+              <div key={p} style={{border: '1px solid black'}} onClick={() => goToPage(starship.url, 'starships')}>
+                <p>{starship.name}</p>
+                <p>{starship.manufacturer}</p>
+                <p>{starship.model}</p>
+                <p>{starship.passengers}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 export default SearchScreen
